7bcd5db29686e9ee28d91f0e45002487b2cd3ea4
Sam Bryce-Smith <49978382+SamBryce-Smith@users.noreply.github.com>
2022-05-06 14:56:08 +0100

    Add specific cluster config for salmon single step

--- submit.sh
+++ submit.sh
@@ -11,6 +11,15 @@
 #$ -j y
 #$ -R y
 
+
+if [[ ( $@ == "--help") ||  $@ == "-h" ]]; then
+    echo "Usage: source submit.sh SMK_NAME RUN_NAME"
+    echo "SMK_NAME - Name of snakemake file (without .smk extension) under single_steps/ to run on cluster"
+    echo "RUN_NAME - Optional argument to name run. Config file for run will be copied to folder containing cluster log files (.submissions/<date><time>/) with run name prefixed"
+    echo "-h/--help - print this help message and exit"
+    exit 0
+fi
+
 WORKFLOW="single_steps/${1}.smk"
 
 if [ "$2" != "" ]; then
@@ -22,13 +31,13 @@ fi
 FOLDER=submissions/$(date +"%Y%m%d%H%M")
 
 mkdir -p ${FOLDER}
-cp config/${1}_config.yaml ${FOLDER}/${RUN_NAME}_config.yaml
+cp config/${1}_config.yaml ${FOLDER}/${RUN_NAME}_${1}_config.yaml
 
 snakemake -s ${WORKFLOW} \
 --conda-prefix "/SAN/vyplab/vyplab_reference_genomes/conda_envs/" \
 --use-conda \
 --jobscript cluster_qsub.sh \
---cluster-config config/cluster.yaml \
+--cluster-config config/cluster/${1}.yaml \
 --cluster-sync "qsub -l tmem={cluster.tmem},h_vmem={cluster.h_vmem},h_rt={cluster.h_rt} -o $FOLDER {cluster.submission_string}" \
 -j 40 \
 --nolock \
